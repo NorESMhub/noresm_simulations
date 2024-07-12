c8ab2d2fcd6dd187603d74776b0dafa93f4001f5
Mariana Vertenstein <marianav@met.no>
2024-07-12 11:26:49 +0200

    Case capture of test_nf1850oslo_trigrid at Fri Jul 12 11:26:48 CEST 2024
    See https://github.com/NorESMhub/noresm_simulations/issues/0

--- replay.sh
+++ replay.sh
@@ -2,173 +2,59 @@
 
 set -e
 
-# Created 2024-06-11 20:43:50
+# Created 2024-06-17 15:02:39
 
-CASEDIR="/cluster/home/mvertens/noresm/test_nf1850oslo"
+CASEDIR="/cluster/home/mvertens/noresm/test_nf1850oslo_trigrid"
 
-/cluster/projects/nn2345k/mvertens/src/noresm2_5_alpha03_cam/cime/scripts/create_newcase --compset NF1850oslo --res ne30pg3_ne30pg3_mtn14 --run-unsupported --case "${CASEDIR}" --project nn9039k
+/cluster/projects/nn2345k/mvertens/src/noresm2_5_alpha03_cam/cime/scripts/create_newcase --case "${CASEDIR}" --compset NF1850oslo --res a%ne30np4.pg3_l%0.9x1.25_oi%ne30np4.pg3_r%r05_w%null_z%null_g%null_m%tnx1v4 --project nn9039k --run-unsupported
 
 cd "${CASEDIR}"
 
-./case.setup
-
-./case.build
-
-./case.setup
-
-./preview_namelists
-
-./preview_namelists --debug
-
-./case.build
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./case.build
-
-./case.build
-
-./case.build
-
-./case.build
-
-./preview_namelists
-
-./case.submit
-
-./preview_namelists
-
-./preview_namelists
-
-./case.submit
-
-./case.build
-
-./case.build
-
-./case.submit
-
-./case.build
-
-./case.build
-
-./case.submit
-
-./case.build
-
-./case.submit
-
-./case.build
-
-./case.build
-
-./case.build
-
-./pelayout
-
-./xmlchange NTASKS=1024
-
-./case.setup --reset
-
-./case.build
-
-./case.build
-
-./case.submit
-
-./pelayout
-
-./pelayout
-
 ./xmlchange NTASKS=1536
 
-./case.setup --reset
+./case.setup
+
+./preview_namelists
 
 ./case.build
 
 ./case.submit
-
-./pelayout
-
-./xmlchange NTASKS=1024
-
-./case.setup --reset
-
-./case.build
-
-./xmlchange DOUT_S=FALSE
-
-./xmlchange REST_N=never
-
-./xmlchange REST_OPTION=never
-
-./xmlchange REST_OPTION=none
-
-./pelayout
-
-./case.submit
-
-./xmlchange TIMER_LEVEL=7
-
-./case.build
-
-./case.submit
-
-./case.build
-
-./case.build
-
-./case.submit
-
-./case.build
-
-./case.submit
-
-./xmlchange DOUT_S=TRUE
 
 ./xmlchange STOP_OPTION=nyears
 
-./xmlchange REST_OPTINO=nyears
+./xmlchange JOB_WALLCLOCK_TIME=24:00:00
 
-./xmlchange REST_OPTION=nyears
+./case.submit
 
-./xmlchange REST_N=5
+./xmlchange STOP_OPTION=nyears
 
-./pelayout
+./xmlchange DEBUG=TRUE
 
-./xmlchange NTASKS=1536
+./case.build --clean-all
 
-./case.setup --reset
+./case.build
+
+./xmlchange HIST_N=1
+
+./xmlchange HIST_OPTION=ndays
+
+./case.submit
+
+./xmlchange HIST_OPTION=monthly
+
+./xmlchange HIST_OPTION=nmonths
+
+./xmlchange HIST_N=1
+
+./xmlchange DEBUG=FALSE
+
+./case.build --clean-all
 
 ./case.build
 
 ./case.submit
 
-./xmlchange JOB_WALLCLOCK_TIME=24:00:00
-
-./case.submit
-
 ./case.build
 
-./pelayout
-
-./xmlchange JOB_WALLCLOCK_TIME=24:00:00
-
 ./case.submit
-
-./pelayout
-
-./xmlchange TIMER_LEVEL=9
-
-./pelayout
-
-./pelayout
 
