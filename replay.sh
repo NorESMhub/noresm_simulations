4e435a681c1b9651ee96c340042cf19906ba24f2
Mariana Vertenstein <marianav@met.no>
2024-07-12 11:27:18 +0200

    Case capture of test_nf1850oslo_zm at Fri Jul 12 11:27:18 CEST 2024
    See https://github.com/NorESMhub/noresm_simulations/issues/0

--- replay.sh
+++ replay.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# Created 2024-06-17 15:02:39
+# Created 2024-06-20 13:12:09
 
-CASEDIR="/cluster/home/mvertens/noresm/test_nf1850oslo_trigrid"
+CASEDIR="/cluster/home/mvertens/noresm/test_nf1850oslo_zm"
 
-/cluster/projects/nn2345k/mvertens/src/noresm2_5_alpha03_cam/cime/scripts/create_newcase --case "${CASEDIR}" --compset NF1850oslo --res a%ne30np4.pg3_l%0.9x1.25_oi%ne30np4.pg3_r%r05_w%null_z%null_g%null_m%tnx1v4 --project nn9039k --run-unsupported
+/cluster/projects/nn2345k/mvertens/src/noresm2_5_alpha03_cam/cime/scripts/create_newcase --compset NF1850oslo --res ne30pg3_ne30pg3_mtn14 --run-unsupported --case "${CASEDIR}" --project nn9039k
 
 cd "${CASEDIR}"
 
@@ -16,45 +16,13 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
-./case.build
+./preview_namelists
 
-./case.submit
+./case.build
 
 ./xmlchange STOP_OPTION=nyears
 
 ./xmlchange JOB_WALLCLOCK_TIME=24:00:00
-
-./case.submit
-
-./xmlchange STOP_OPTION=nyears
-
-./xmlchange DEBUG=TRUE
-
-./case.build --clean-all
-
-./case.build
-
-./xmlchange HIST_N=1
-
-./xmlchange HIST_OPTION=ndays
-
-./case.submit
-
-./xmlchange HIST_OPTION=monthly
-
-./xmlchange HIST_OPTION=nmonths
-
-./xmlchange HIST_N=1
-
-./xmlchange DEBUG=FALSE
-
-./case.build --clean-all
-
-./case.build
-
-./case.submit
-
-./case.build
 
 ./case.submit
 
