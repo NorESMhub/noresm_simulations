05b891c775de7e7d5dc811c28e9db499b8872dbe
Mariana Vertenstein <marianav@met.no>
2024-07-24 15:15:41 +0200

    Case capture of n1850.ne30_f09_tn14.twin_isopycnic.20240712 at Wed Jul 24 15:15:40 CEST 2024
    See https://github.com/NorESMhub/noresm_simulations/issues/0

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2024-07-12 14:07:19
+# Created 2024-07-12 14:07:01
 
-CASEDIR="/cluster/home/mvertens/cases/n1850.ne30_f09_tn14.twin_hybrid.20240712"
+CASEDIR="/cluster/home/mvertens/cases/n1850.ne30_f09_tn14.twin_isopycnic.20240712"
 
 /cluster/projects/nn9560k/mvertens/src/noresm2_5_alpha03/cime/scripts/create_newcase --case "${CASEDIR}" --compset N1850 --res ne30pg3_f09_tn14 --project nn9039k --run-unsupported
 
@@ -32,13 +32,19 @@ cd "${CASEDIR}"
 
 ./xmlchange JOB_WALLCLOCK_TIME=24:00:00
 
-./xmlchange BLOM_VCOORD=cntiso_hybrid,BLOM_TURBULENT_CLOSURE=
-
 ./case.setup --reset
 
 ./case.build
 
+./xmlchange STOP_N=10
+
+./xmlchange JOB_WALLCLOCK_TIME=36:00:00
+
+./case.submit
+
 ./xmlchange STOP_N=6
+
+./xmlchange JOB_WALLCLOCK_TIME=24:00:00
 
 ./xmlchange RESUBMIT=0
 
@@ -46,15 +52,9 @@ cd "${CASEDIR}"
 
 ./xmlchange CONTINUE_RUN=TRUE
 
-./case.submit
-
-./xmlchange STOP_N=4
+./xmlchange STOP_N=2
 
 ./case.submit
 
-./xmlchange DOUT_S_ROOT=/nird/datalake/NS2345k/mvertens/n1850.ne30_f09_tn14.twin_hybrid.20240712
-
-./xmlchange DOUT_S_ROOT=/nird/datalake/NS2345K/mvertens/n1850.ne30_f09_tn14.twin_hybrid.20240712
-
-./pelayout
+./xmlchange DOUT_S_ROOT=/nird/datalake/NS2345K/mvertens/n1850.ne30_f09_tn14.twin_isopycnic.20240712
 
