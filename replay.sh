09a064cced37b40a2a7161196161c7d5c003adfd
Mariana Vertenstein <marianav@met.no>
2024-07-24 15:39:41 +0200

    Case capture of n1850.ne30_f09_tn14.twin_hybrid_coare.20240723 at Wed Jul 24 15:39:41 CEST 2024
    See https://github.com/NorESMhub/noresm_simulations/issues/10

--- replay.sh
+++ replay.sh
@@ -2,59 +2,93 @@
 
 set -e
 
-# Created 2024-07-12 14:07:01
+# Created 2024-07-23 11:00:28
 
-CASEDIR="/cluster/home/mvertens/cases/n1850.ne30_f09_tn14.twin_isopycnic.20240712"
+CASEDIR="/cluster/home/mvertens/cases/n1850.ne30_f09_tn14.twin_hybrid_coare.20240723"
 
 /cluster/projects/nn9560k/mvertens/src/noresm2_5_alpha03/cime/scripts/create_newcase --case "${CASEDIR}" --compset N1850 --res ne30pg3_f09_tn14 --project nn9039k --run-unsupported
 
 cd "${CASEDIR}"
 
-./case.setup
+./pelayout
 
 ./xmlchange NTASKS=1920
 
-./xmlchange ROOTPE=0
-
-./xmlchange NTASKS_OCN=128
+./xmlchange NTASKS_OCN=256
 
 ./xmlchange ROOTPE_OCN=1920
 
+./xmlchange BLOM_VCOORD=cntiso_hybrid,BLOM_TURBULENT_CLOSURE=
+
+./xmlchange STOP_N=10
+
 ./xmlchange STOP_OPTION=nyears
 
-./xmlchange STOP_N=5
+./xmlchange DOUT_S_ROOT=/nird/datalake/NS2345K/mvertens/twin_hybrid_coare.20240723
+
+./case.setup
+
+./case.build
+
+./xmlchange JOB_WALLCLOCK_TIME=1-18:00:00
+
+./case.submit
+
+./xmlchange JOB_WALLCLOCK_TIMTE=1D-18:00:00
+
+./xmlchange JOB_WALLCLOCK_TIME=1D-18:00:00
+
+./case.submit
+
+./xmlchange JOB_WALLCLOCK_TIME=1d-18:00:00
+
+./case.submit
+
+./xmlchange JOB_WALLCLOCK_TIME=1-18:0:0
+
+./case.submit
+
+./xmlchange JOB_WALLCLOCK_TIME=2-0:0:0
+
+./case.submit
 
 ./xmlchange REST_N=1
 
 ./xmlchange REST_OPTION=nyears
 
-./xmlchange RESUBMIT=1
-
 ./xmlchange JOB_WALLCLOCK_TIME=24:00:00
-
-./case.setup --reset
-
-./case.build
-
-./xmlchange STOP_N=10
-
-./xmlchange JOB_WALLCLOCK_TIME=36:00:00
 
 ./case.submit
 
 ./xmlchange STOP_N=6
 
-./xmlchange JOB_WALLCLOCK_TIME=24:00:00
+./case.submit
 
-./xmlchange RESUBMIT=0
+./xmlchange STOP_N=10
+
+./xmlchange JOB_WALLCLOCK_TIME=2d-0:0:0
+
+./xmlchange --subgroup case.st_archive JOB_WALLCLOCK_TIME=00:20:00
 
 ./case.submit
+
+./case.submit
+
+./xmlchange JOB_WALLCLOCK_TIME=1-18:0:0
+
+./case.submit
+
+./xmlchange JOB_WALLCLOCK_TIME=24:00:00
+
+./xmlchange STOP_N=6
+
+./case.submit
+
+./xmlchange STOP_N=4
+
+./xmlchange CONTINUE_RUN=TRUe
 
 ./xmlchange CONTINUE_RUN=TRUE
 
-./xmlchange STOP_N=2
-
 ./case.submit
-
-./xmlchange DOUT_S_ROOT=/nird/datalake/NS2345K/mvertens/n1850.ne30_f09_tn14.twin_isopycnic.20240712
 
